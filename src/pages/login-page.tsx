5453b43fa910fcab9194627d7bdfbc26952e16d6
Dan Carter <djcarter85@gmail.com>
2024-05-29 13:00:11 +0100

    feat: add some design to the login page

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -1,6 +1,28 @@
 import { signIn } from "aws-amplify/auth";
 import { useState } from "react";
 
+const Input = ({
+  type,
+  placeholder,
+  value,
+  setValue,
+}: {
+  type: "email" | "password";
+  placeholder: string;
+  value: string;
+  setValue: (v: string) => void;
+}) => {
+  return (
+    <input
+      className="rounded px-3 py-2 border border-gray-500/80"
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={(ev) => setValue(ev.target.value)}
+    />
+  );
+};
+
 const LoginPage = () => {
   const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
@@ -10,26 +32,23 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <div>
-        <input
+    <div className="relative flex min-h-screen flex-col justify-center">
+      <div className="mx-auto my-8 flex w-96 flex-col gap-4 rounded-lg border border-gray-900/20 shadow-xl bg-white p-8">
+        <Input
           type="email"
           placeholder="Email"
           value={emailAddress}
-          onChange={(ev) => setEmailAddress(ev.target.value)}
+          setValue={setEmailAddress}
         />
-      </div>
-      <div>
-        <input
+        <Input
           type="password"
           placeholder="Password"
           value={password}
-          onChange={(ev) => setPassword(ev.target.value)}
+          setValue={setPassword}
         />
-      </div>
-      <div>
         <input
           type="submit"
+          className="rounded bg-sky-500 p-2 font-bold text-sky-50 hover:bg-sky-600"
           onClick={(e) => {
             e.preventDefault();
             logIn();
