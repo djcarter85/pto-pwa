a50b7fb67c0376df49b71e8f3db665c5a253927a
Dan Carter <djcarter85@gmail.com>
2024-09-23 19:45:47 +0100

    feat: add actual auth using Cognito

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -1,18 +1,50 @@
 import { useNavigate } from "react-router-dom";
 import { logIn } from "../services/auth-service";
 import { getDashboardHomeUrl } from "../utils/urls";
+import { useState } from "react";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const navigate = useNavigate();
 
-  const onLogInClick = () => {
-    logIn();
-    navigate(getDashboardHomeUrl());
+  const onLogInClick = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    
+    const result = await logIn(email, password);
+    if (result) {
+      navigate(getDashboardHomeUrl());
+    }
   };
 
   return (
     <div>
-      <button onClick={onLogInClick}>Log in</button>
+      <form onSubmit={onLogInClick}>
+        <div>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+          />
+        </div>
+        <div>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Log in</button>
+        </div>
+      </form>
     </div>
   );
 };
--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,13 +1,45 @@
+import {
+  AuthFlowType,
+  CognitoIdentityProviderClient,
+  InitiateAuthCommand,
+} from "@aws-sdk/client-cognito-identity-provider";
+import config from "../config.json";
+
+const cognitoClient = new CognitoIdentityProviderClient({
+  region: config.region,
+});
+
 const isLoggedIn = () => {
-  return !!localStorage.getItem("isLoggedIn");
+  return !!localStorage.getItem("accessToken");
 };
 
-const logIn = () => {
-  localStorage.setItem("isLoggedIn", "true");
+const logIn = async (email: string, password: string) => {
+  const command = new InitiateAuthCommand({
+    AuthFlow: AuthFlowType.USER_PASSWORD_AUTH,
+    ClientId: config.clientId,
+    AuthParameters: {
+      USERNAME: email,
+      PASSWORD: password,
+    },
+  });
+
+  const { AuthenticationResult: result } = await cognitoClient.send(command);
+
+  if (result) {
+    localStorage.setItem("idToken", result.IdToken || "");
+    localStorage.setItem("accessToken", result.AccessToken || "");
+    localStorage.setItem("refreshToken", result.RefreshToken || "");
+
+    return !!result.AccessToken;
+  } else {
+    return false;
+  }
 };
 
 const logOut = () => {
-  localStorage.removeItem("isLoggedIn");
+  localStorage.removeItem("idToken");
+  localStorage.removeItem("accessToken");
+  localStorage.removeItem("refreshToken");
 };
 
 export { isLoggedIn, logIn, logOut };
