17dbff3797b3fe6a4422c1d0efb53be65c7db84b
Dan Carter <djcarter85@gmail.com>
2024-09-30 15:14:24 +0100

    feat: add support for refresh token

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -4,16 +4,60 @@ import {
   InitiateAuthCommand,
 } from "@aws-sdk/client-cognito-identity-provider";
 import config from "../config.json";
+import { jwtDecode } from "jwt-decode";
+import { DateTime } from "luxon";
 
 const cognitoClient = new CognitoIdentityProviderClient({
   region: config.region,
 });
 
-const isLoggedIn = () => {
-  return !!localStorage.getItem("accessToken");
+export const isLoggedIn = () => {
+  return !!localStorage.getItem("idToken");
 };
 
-const logIn = async (email: string, password: string) => {
+export const getSession = async () => {
+  let idToken = localStorage.getItem("idToken");
+  let refreshToken = localStorage.getItem("refreshToken");
+
+  console.log(idToken);
+
+  if (!idToken || !refreshToken) {
+    return null;
+  }
+
+  const decodedIdToken = jwtDecode(idToken);
+  console.log(decodedIdToken);
+
+  if (!decodedIdToken.exp) {
+    return null;
+  }
+
+  const expiry = DateTime.fromSeconds(decodedIdToken.exp);
+
+  if (DateTime.now() > expiry.minus({ minutes: 1 })) {
+    const command = new InitiateAuthCommand({
+      AuthFlow: AuthFlowType.REFRESH_TOKEN_AUTH,
+      ClientId: config.clientId,
+      AuthParameters: {
+        REFRESH_TOKEN: refreshToken,
+      },
+    });
+
+    const { AuthenticationResult: result } = await cognitoClient.send(command);
+
+    idToken = result?.IdToken ?? null;
+    localStorage.setItem("idToken", idToken ?? "");
+
+    if (result?.RefreshToken) {
+      refreshToken = result?.RefreshToken;
+      localStorage.setItem("refreshToken", refreshToken);
+    }
+  }
+
+  return { idToken };
+};
+
+export const logIn = async (email: string, password: string) => {
   const command = new InitiateAuthCommand({
     AuthFlow: AuthFlowType.USER_PASSWORD_AUTH,
     ClientId: config.clientId,
@@ -27,19 +71,15 @@ const logIn = async (email: string, password: string) => {
 
   if (result) {
     localStorage.setItem("idToken", result.IdToken || "");
-    localStorage.setItem("accessToken", result.AccessToken || "");
     localStorage.setItem("refreshToken", result.RefreshToken || "");
 
-    return !!result.AccessToken;
+    return !!result.IdToken;
   } else {
     return false;
   }
 };
 
-const logOut = () => {
+export const logOut = () => {
   localStorage.removeItem("idToken");
-  localStorage.removeItem("accessToken");
   localStorage.removeItem("refreshToken");
 };
-
-export { isLoggedIn, logIn, logOut };
