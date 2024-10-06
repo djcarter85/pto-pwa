608ac63a1c438a9c1b87b940d28f1f73b939e855
Dan Carter <djcarter85@gmail.com>
2024-10-06 20:33:17 +0100

    feat: improve design of loading icon

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,5 +1,12 @@
+import cx from "classix";
 import { ReactNode } from "react";
 
-export const Container = ({ children }: { children: ReactNode }) => {
-  return <div className="mx-auto max-w-xl">{children}</div>;
+export const Container = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => {
+  return <div className={cx("mx-auto max-w-xl", className)}>{children}</div>;
 };
