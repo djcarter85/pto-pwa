ca036fdbe97ba743dfa6bbc2f839d78cca2ceac8
Dan Carter <djcarter85@gmail.com>
2025-03-30 20:02:08 +0100

    feat: improve log in page (#4)

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { logIn } from "../services/auth-service";
 import { getDashboardHomeUrl } from "../utils/urls";
 import { useState } from "react";
+import { Container } from "../components/container";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -20,31 +21,46 @@ const LoginPage = () => {
 
   return (
     <main className="py-safe">
-      <form onSubmit={onLogInClick}>
-        <div>
-          <input
-            id="email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email"
-            required
-          />
-        </div>
-        <div>
-          <input
-            id="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            required
-          />
-        </div>
-        <div>
-          <button type="submit">Log in</button>
-        </div>
-      </form>
+      <Container className="flex min-h-dvh flex-col justify-center px-6">
+        <form onSubmit={onLogInClick} className="space-y-6">
+          <div>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              required
+              autoComplete="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-neutral-900 outline-1 -outline-offset-1 outline-neutral-300 placeholder:text-neutral-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 sm:text-sm/6"
+            />
+          </div>
+
+          <div>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              placeholder="Password"
+              autoComplete="current-password"
+              className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-neutral-900 outline-1 -outline-offset-1 outline-neutral-300 placeholder:text-neutral-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 sm:text-sm/6"
+            />
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+            >
+              Log in
+            </button>
+          </div>
+        </form>
+      </Container>
     </main>
   );
 };
