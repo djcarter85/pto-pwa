ccb19e5d13d9ae1bfcd4329483a2ded8313de644
Dan Carter <djcarter85@gmail.com>
2025-03-31 21:36:15 +0100

    feat: go back to using DM Sans

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import "@fontsource/archivo/400.css";
-import "@fontsource/archivo/700.css";
+import "@fontsource/dm-sans/400.css";
+import "@fontsource/dm-sans/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
