b8a4b8e63f0b6881b61446b9baa34deddef3efb4
Dan Carter <djcarter85@gmail.com>
2025-04-01 21:49:54 +0100

    refactor: remove unused fonts

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import "@fontsource/archivo/400.css";
-import "@fontsource/archivo/700.css";
-import "@fontsource/dm-sans/400.css";
-import "@fontsource/dm-sans/700.css";
 import "@fontsource/mulish/400.css";
 import "@fontsource/mulish/700.css";
-import "@fontsource/noto-sans/400.css";
-import "@fontsource/noto-sans/700.css";
-import "@fontsource/open-sans/400.css";
-import "@fontsource/open-sans/700.css";
-import "@fontsource/poppins/400.css";
-import "@fontsource/poppins/700.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
