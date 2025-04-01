9403448d368e6c4c64affc2c06e642054c5beec8
Dan Carter <djcarter85@gmail.com>
2025-04-01 21:50:11 +0100

    feat: improve design of buttons

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 import "@fontsource/mulish/400.css";
+import "@fontsource/mulish/600.css";
 import "@fontsource/mulish/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
