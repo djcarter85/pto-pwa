14e1263bec6e54e0473759edea8a37e0769def8f
Dan Carter <djcarter85@gmail.com>
2025-04-01 22:26:44 +0100

    refactor: extract component (#7)

--- src/pages/dashboard/predictions-page.tsx
+++ src/pages/dashboard/predictions-page.tsx
@@ -5,6 +5,7 @@ import { formatDate } from "../../utils/formats";
 import { Fragment } from "react";
 import { Header } from "../../components/header";
 import { Input } from "../../components/input";
+import { DateTime } from "luxon";
 
 const TeamImage = ({ teamId }: { teamId: number }) => {
   return <img src={`/assets/teams/logo-${teamId}.svg`} className="size-8" />;
@@ -57,6 +58,55 @@ const getPointsText = (points: number | null) => {
   return `${points} pts`;
 };
 
+const MatchBlock = ({
+  matchPrediction,
+}: {
+  matchPrediction: {
+    match: {
+      id: string;
+      homeTeam: { id: number; name: string };
+      awayTeam: { id: number; name: string };
+      kickoff: DateTime;
+      finalScore: {
+        home: number;
+        away: number;
+      } | null;
+    };
+    predictedScore: {
+      home: number;
+      away: number;
+    } | null;
+    points: number | null;
+  };
+}) => {
+  return (
+    <div className="col-span-5 grid grid-cols-subgrid items-center gap-x-3 gap-y-1 px-3 py-2">
+      <TeamImage teamId={matchPrediction.match.homeTeam.id} />
+      <TeamName teamName={matchPrediction.match.homeTeam.name} />
+      <ScoreInput
+        type="home"
+        matchId={matchPrediction.match.id}
+        initialValue={matchPrediction.predictedScore?.home}
+      />
+      <ScoreValue score={matchPrediction.match.finalScore?.home} />
+      <div className="text-center text-sm">
+        {matchPrediction.match.kickoff.toFormat("HH:mm")}
+      </div>
+      <TeamImage teamId={matchPrediction.match.awayTeam.id} />
+      <TeamName teamName={matchPrediction.match.awayTeam.name} />
+      <ScoreInput
+        type="away"
+        matchId={matchPrediction.match.id}
+        initialValue={matchPrediction.predictedScore?.away}
+      />
+      <ScoreValue score={matchPrediction.match.finalScore?.away} />
+      <div className="text-center text-sm">
+        {getPointsText(matchPrediction.points)}
+      </div>
+    </div>
+  );
+};
+
 export const PredictionsPage = () => {
   const { data, error } = useQuery({
     queryKey: ["predictions"],
@@ -93,33 +143,7 @@ export const PredictionsPage = () => {
                     b.match.kickoff.toUnixInteger(),
                 )
                 .map((mp) => (
-                  <div
-                    key={mp.match.id}
-                    className="col-span-5 grid grid-cols-subgrid items-center gap-x-3 gap-y-1 px-3 py-2"
-                  >
-                    <TeamImage teamId={mp.match.homeTeam.id} />
-                    <TeamName teamName={mp.match.homeTeam.name} />
-                    <ScoreInput
-                      type="home"
-                      matchId={mp.match.id}
-                      initialValue={mp.predictedScore?.home}
-                    />
-                    <ScoreValue score={mp.match.finalScore?.home} />
-                    <div className="text-center text-sm">
-                      {mp.match.kickoff.toFormat("HH:mm")}
-                    </div>
-                    <TeamImage teamId={mp.match.awayTeam.id} />
-                    <TeamName teamName={mp.match.awayTeam.name} />
-                    <ScoreInput
-                      type="away"
-                      matchId={mp.match.id}
-                      initialValue={mp.predictedScore?.away}
-                    />
-                    <ScoreValue score={mp.match.finalScore?.away} />
-                    <div className="text-center text-sm">
-                      {getPointsText(mp.points)}
-                    </div>
-                  </div>
+                  <MatchBlock key={mp.match.id} matchPrediction={mp} />
                 ))}
             </Fragment>
           ))}
