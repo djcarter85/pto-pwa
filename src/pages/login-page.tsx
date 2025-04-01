54f2274aea18a22eae3172796e6729dbabe844c6
Dan Carter <djcarter85@gmail.com>
2025-04-01 21:29:13 +0100

    refactor: use button instead of form (#4)

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -23,46 +23,44 @@ const LoginPage = () => {
 
   return (
     <main className="py-safe">
-      <Container className="flex min-h-dvh flex-col justify-center px-6">
-        <form onSubmit={onLogInClick} className="space-y-4">
-          <div className="flex justify-center">
-            <img src={logo} className="size-24 rounded-lg" />
-          </div>
-          <div>
-            <Input
-              id="email"
-              type="email"
-              value={email}
-              setValue={setEmail}
-              autoComplete="email"
-              placeholder="Email"
-              required={true}
-              className="block w-full px-3 py-1.5"
-            />
-          </div>
+      <Container className="flex min-h-dvh flex-col justify-center space-y-4 px-6">
+        <div className="flex justify-center">
+          <img src={logo} className="size-24 rounded-lg" />
+        </div>
+        <div>
+          <Input
+            id="email"
+            type="email"
+            value={email}
+            setValue={setEmail}
+            autoComplete="email"
+            placeholder="Email"
+            required={true}
+            className="block w-full px-3 py-1.5"
+          />
+        </div>
 
-          <div>
-            <Input
-              id="password"
-              type="password"
-              value={password}
-              setValue={setPassword}
-              autoComplete="current-password"
-              placeholder="Password"
-              required={true}
-              className="block w-full px-3 py-1.5"
-            />
-          </div>
+        <div>
+          <Input
+            id="password"
+            type="password"
+            value={password}
+            setValue={setPassword}
+            autoComplete="current-password"
+            placeholder="Password"
+            required={true}
+            className="block w-full px-3 py-1.5"
+          />
+        </div>
 
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-            >
-              Log in
-            </button>
-          </div>
-        </form>
+        <div>
+          <button
+            onClick={onLogInClick}
+            className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+          >
+            Log in
+          </button>
+        </div>
       </Container>
     </main>
   );
