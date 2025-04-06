34c0c2ef379b08581339853c22bb5147e6dd8309
Dan Carter <djcarter85@gmail.com>
2025-04-06 21:07:33 +0100

    feat: add idea for title bar

---
+++ src/components/title-bar.tsx
@@ -0,0 +1,10 @@
+export const TitleBar = () => {
+  return (
+    <header className="pt-safe fixed top-0 right-0 left-0 grid h-12 grid-cols-[auto_1fr] items-center gap-x-3 bg-white px-3">
+      <div className="bg-primary-500 flex size-8 items-center justify-center rounded-full text-white">
+        P
+      </div>
+      <div className="text-xl font-semibold">pto.football</div>
+    </header>
+  );
+};
--- src/layouts/dashboard-layout.tsx
+++ src/layouts/dashboard-layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Nav } from "../components/nav";
 import { Container } from "../components/container";
+import { TitleBar } from "../components/title-bar";
 
 const DashboardLayout = () => {
   return (
-    <main className="pt-safe pb-safe-offset-16">
+    <main className="pt-safe-offset-12 pb-safe-offset-16">
+      <TitleBar />
       <Container>
         <Outlet />
       </Container>
