7244c12005dba8043f03b6d7daffb9e578698e35
Dan Carter <djcarter85@gmail.com>
2025-04-06 21:01:09 +0100

    feat: improve spacing on Leaderboard page

--- src/pages/dashboard/leaderboard-page.tsx
+++ src/pages/dashboard/leaderboard-page.tsx
@@ -3,6 +3,7 @@ import { getLeaderboardPage } from "../../services/pto-api-service";
 import cx from "classix";
 import { Loading } from "../../components/loading";
 import { Header } from "../../components/header";
+import { Fragment } from "react";
 
 export const LeaderboardPage = () => {
   const { data, error } = useQuery({
@@ -24,42 +25,36 @@ export const LeaderboardPage = () => {
         tournamentName={data.tournament.shortName}
         roundName={data.round?.shortName ?? "Overall"}
       />
-      <table className="mb-3 w-full">
-        <thead>
-          <tr className="">
-            <th className="px-3 py-1 text-left">#</th>
-            <th className="px-3 py-1 text-left">Player</th>
-            <th className="px-3 py-1 text-center">MP</th>
-            <th className="px-3 py-1 text-center">PTS</th>
-            <th className="px-3 py-1 text-center">AVG</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.leaderboard.map((x) => (
-            <tr key={x.player.id} className="odd:bg-neutral-100">
-              <td className="px-3 py-1 text-left">
-                {x.rank}
-                {x.rankIsShared && "="}
-              </td>
-              <td
-                className={cx(
-                  "px-3 py-0.5 text-left text-lg",
-                  x.player.isHuman || "italic",
-                )}
-              >
-                {x.player.name}
-              </td>
-              <td className="px-3 py-1 text-center">{x.matchesPredicted}</td>
-              <td className="text-primary-700 px-3 py-0.5 text-center text-lg font-bold">
-                {x.totalPoints}
-              </td>
-              <td className="px-3 py-1 text-center">
-                {x.pointsPerMatch.toFixed(2)}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="mb-3 grid w-full grid-cols-[auto_1fr_auto_auto_auto] gap-x-3">
+        <div className="py-1 pl-3 text-left">#</div>
+        <div className="py-1 text-left">Player</div>
+        <div className="py-1 text-center">MP</div>
+        <div className="py-1 text-center">PTS</div>
+        <div className="py-1 pr-3 text-center">AVG</div>
+        {data.leaderboard.map((x) => (
+          <Fragment key={x.player.id}>
+            <div className="py-1 pl-3 text-left">
+              {x.rank}
+              {x.rankIsShared && "="}
+            </div>
+            <div
+              className={cx(
+                "py-0.5 text-left text-lg",
+                x.player.isHuman || "italic",
+              )}
+            >
+              {x.player.name}
+            </div>
+            <div className="py-1 text-center">{x.matchesPredicted}</div>
+            <div className="text-primary-700 py-0.5 text-center text-lg font-bold">
+              {x.totalPoints}
+            </div>
+            <div className="py-1 pr-3 text-center">
+              {x.pointsPerMatch.toFixed(2)}
+            </div>
+          </Fragment>
+        ))}
+      </div>
     </div>
   );
 };
